b594a51fa987acb044a8293781c2693ccf28f0c6
root <root@debian>
2016-05-28 17:48:02 -0400

    pseudo working version

--- p2/example.cpp
+++ p2/example.cpp
@@ -16,7 +16,7 @@ struct thread_args
 	int i2;
 
 };
-struct thread_args t_args;
+
 pthread_mutex_t mutex, valtex;
 pthread_cond_t cond;
 
@@ -36,28 +36,29 @@ int main(int argc, char *argv[])
 	pthread_mutex_init(&mutex, NULL);
 	pthread_mutex_init(&valtex, NULL);
 	pthread_cond_init(&cond, NULL);
-	int integers[4096];
+	int integers[8];
 	int i = 0, tmp;
-	std::ifstream read(argv[1]);
+	std::ifstream read("inData.txt");
 	while(read >> tmp) {
 		workSet.push_back(tmp);
 		i++;
 	}
-	read.close();
 	printf("%d\n", i);
-	int rounds;
+	int rounds = Log2(i);
 	int to_process = i;
 	pthread_t t_ids[i/2];
 	int thread_size = 0;
-	rounds = Log2(i);
+	struct thread_args *t_args;
+	//rounds = Log2(i);
 	//workSet.assign(integers, integers+i);
 	while(rounds != 0) {
 		remaining = (to_process)/2;
 		for (int val = 0; val < to_process; val+=2) {
-			t_args.i1 = workSet[val];
-			t_args.i2 = workSet[val+1];
+			t_args = malloc(sizeof(struct thread_args));
+			(*t_args).i1 = workSet[val];
+			(*t_args).i2 = workSet[val+1];
 			//printf("i1: %d\ni2: %d\n", t_args.i1, t_args.i2);
-			pthread_create(&t_ids[thread_size], NULL, find_max, NULL);
+			pthread_create(&t_ids[thread_size], NULL, find_max, (void*)t_args);
 			thread_size++;
 		}
 		if (ret == 0) {
@@ -73,10 +74,11 @@ int main(int argc, char *argv[])
 	return 0;	
 }
 
-void* find_max(void* params) {	
+void* find_max(void *params) {
+	struct thread_args *t_args = (struct thread_args*)params;
+	int a = (*t_args).i1;
+	int b = (*t_args).i2;
 	pthread_mutex_lock(&valtex);
-	int a = t_args.i1;
-	int b = t_args.i2;
 	if (a > b) {
 		curSet.push_back(a);
 	} else {
@@ -96,8 +98,10 @@ void* find_max(void* params) {
 		pthread_mutex_lock(&mutex);
 		ret = pthread_cond_broadcast(&cond);
 	}
-	else
+	else {
 		while (remaining != 0) {pthread_cond_wait(&cond, &mutex);}
+	}
 	pthread_mutex_unlock(&mutex);
-	pthread_exit(NULL);
+	free (params);
+	pthread_exit(NULL);	
 }
